80b13eb9d1caeb874df5080ba51105867a0c542a
Kasvit <kasvit93@gmail.com>
2017-07-19 05:27:38 -0700

    createt user1

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 describe "User pages" do
 
-  subject { page }
+   subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 
   describe "signup page" do
     before { visit signup_path }
