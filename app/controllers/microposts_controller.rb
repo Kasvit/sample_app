a33891ec76050c63ebf15d5d6be988afbcc2c01e
Kasvit <kasvit93@gmail.com>
2017-09-04 11:58:40 -0700

    Finished book

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,31 +1,35 @@
 class MicropostsController < ApplicationController
-	before_action :signed_in_user, only: [:create, :destroy]
-	before_action :correct_user, only: :destroy
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
-	def create
-		@micropost = current_user.microposts.build(microposts_params)
-		if @micropost.save
-			flash[:success] = "Пост створено!"
-			redirect_to root_url
-		else
-			@feed_items = []
-			render 'static_pages/home'
+  def new
+    @micropost = Micropost.new
+  end
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Пост створено!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
     end
-	end
+  end
 
-	def destroy
-		@micropost.destroy
-		redirect_to root_url
-	end
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
 
-private
+  private
 
-	def microposts_params
-  	params.require(:micropost).permit(:content)
-	end
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 
-	def correct_user
-		@micropost = current_user.microposts.find_by(id: params[:id])
-		redirect_to root_url if @micropost.nil?
-	end
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
